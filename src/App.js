75b34f72f104556510fdd5e7d8de08f6065109ea
Laurie McLaughlin <lnmclaughlin@gmail.com>
2023-12-29 16:19:34 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,57 +1,7 @@
 import "./App.css";
-import React, { useState } from "react";
-
-const locations = {
-  region: "",
-  city: "",
-  state: "",
-  zipcode: [""],
-};
 
 function App() {
-  const [zipCodeSearch, setZipCodeSearch] = useState(""); //rename this to [locationSearch, setLocationSearch] ?
-
-  //once user inputs zipcode into search, page should redirect to information on specific zipcode/region
-  //should user be able to search by city and state as well as zipcode?
-  //will the information available be specific enough to zipcode or only cover the region that zipcode is in?
-
-  const handleZipCodeSearch = (e) => {
-    //TODO
-    e.preventDefault();
-  };
-
-  return (
-    <div className="App">
-      {/* replica of navigation for adding the zipcode/location search.
-      the onClick should lead user to a new page with that specific locations information  */}
-
-      <div className="nav-container">
-        <div className="nav-menu">
-          <img
-            className="logo"
-            src="/assets/61520fa346e5a67b68fd5039_justair-logo-full-color-rgb.svg"
-            alt="logo"
-          />
-          <div className="search-bar">
-            <>
-              <input
-                type="text"
-                name="zip-code"
-                className="zip"
-                placeholder="search by zipcode"
-                value={zipCodeSearch}
-              />
-              <button className="searchbtn" onClick={handleZipCodeSearch}>
-                Search
-              </button>
-            </>
-
-            <i class="fa-solid fa-bars"></i>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,54 @@
+import React, { useState } from "react";
+
+const locations = {
+  region: "",
+  city: "",
+  state: "",
+  zipcode: [""],
+};
 const SearchBar = () => {
-  return <div className="name">name works</div>;
+  const [zipCodeSearch, setZipCodeSearch] = useState(""); //rename this to [locationSearch, setLocationSearch] ?
+
+  //once user inputs zipcode into search, page should redirect to information on specific zipcode/region
+  //should user be able to search by city and state as well as zipcode?
+  //will the information available be specific enough to zipcode or only cover the region that zipcode is in?
+
+  const handleZipCodeSearch = (e) => {
+    //TODO
+    e.preventDefault();
+  };
+
+  {
+    /* replica of navigation for adding the zipcode/location search.
+the onClick should lead user to a new page with that specific locations information  */
+  }
+  return (
+    <div className="nav-container">
+      <div className="nav-menu">
+        <img
+          className="logo"
+          src="/assets/61520fa346e5a67b68fd5039_justair-logo-full-color-rgb.svg"
+          alt="logo"
+        />
+        <div className="search-bar">
+          <>
+            <input
+              type="text"
+              name="zip-code"
+              className="zip"
+              placeholder="search by zipcode"
+              value={zipCodeSearch}
+            />
+            <button className="searchbtn" onClick={handleZipCodeSearch}>
+              Search
+            </button>
+          </>
+
+          <i class="fa-solid fa-bars"></i>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
