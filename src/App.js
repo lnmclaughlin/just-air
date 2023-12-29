41627b406d546cedbccbd9cb7e08e6692023e5b6
Laurie McLaughlin <lnmclaughlin@gmail.com>
2023-12-29 14:51:46 -0500

    form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
 
-const defaultFormFields = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  subject: "",
-  message: "",
-};
-
 const locations = {
   region: "",
   city: "",
@@ -17,21 +9,7 @@ const locations = {
 };
 
 function App() {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { firstName, lastName, email, subject, message } = formFields;
-  const [buttonText, setButtonText] = useState("Submit");
   const [zipCodeSearch, setZipCodeSearch] = useState(""); //rename this to [locationSearch, setLocationSearch] ?
-
-  const resetFormFields = () => {
-    //TODO
-    setFormFields(defaultFormFields);
-  };
-
-  const handleButtonClick = () => {
-    // Change the button text when clicked
-    // would be cool if this changed from a button to a checkmark once clicked instead of just saying "Thanks!"
-    setButtonText("Thanks!");
-  };
 
   //once user inputs zipcode into search, page should redirect to information on specific zipcode/region
   //should user be able to search by city and state as well as zipcode?
@@ -40,18 +18,6 @@ function App() {
   const handleZipCodeSearch = (e) => {
     //TODO
     e.preventDefault();
-  };
-
-  //Form code: do you want users to have the capability to give feedback as a guest or do they need and account?
-
-  const handleFormSubmit = (event) => {
-    //TODO
-    event.preventDefault();
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormFields({ ...formFields, [name]: value });
   };
 
   return (
@@ -83,72 +49,6 @@ function App() {
             <i class="fa-solid fa-bars"></i>
           </div>
         </div>
-      </div>
-
-      {/* form component starts here */}
-      <h2>Contact Us</h2>
-
-      <div className="form-container">
-        <form onSubmit={handleFormSubmit}>
-          <h3>First Name</h3>
-          <label className="name">
-            <input
-              type="text"
-              name="firstname"
-              placeholder="First Name"
-              required
-              onChange={handleInputChange}
-              value={firstName}
-            />
-          </label>
-          <h3>Last Name</h3>
-          <label className="name">
-            <input
-              type="text"
-              name="lastname"
-              placeholder="Last Name"
-              required
-              onChange={handleInputChange}
-              value={lastName}
-            />
-          </label>
-          <h3>Email</h3>
-          <label className="email">
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-              onChange={handleInputChange}
-              value={email}
-            />
-          </label>
-          <h3>Subject</h3>
-          <label className="subject">
-            <input
-              type="subject"
-              name="subject"
-              placeholder="Subject"
-              required
-              onChange={handleInputChange}
-              value={subject}
-            />
-          </label>
-          <h3>Message</h3>
-          <div className="message-container">
-            <textarea
-              type="text"
-              name="message"
-              placeholder="Message"
-              required
-              onChange={handleInputChange}
-              value={message}
-            />
-          </div>
-          <button type="submit" className="formbtn" onClick={handleButtonClick}>
-            {buttonText}
-          </button>
-        </form>
       </div>
     </div>
   );
---
+++ src/components/Form.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+
+export const defaultFormFields = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  subject: "",
+  message: "",
+};
+
+export const Form = () => {
+  const { firstName, lastName, email, subject, message } = formFields;
+  const [buttonText, setButtonText] = useState("Submit");
+  const [formFields, setFormFields] = useState(defaultFormFields);
+
+  const resetFormFields = () => {
+    //TODO
+    setFormFields(defaultFormFields);
+  };
+
+  const handleButtonClick = () => {
+    // Change the button text when clicked
+    // would be cool if this changed from a button to a checkmark once clicked instead of just saying "Thanks!"
+    setButtonText("Thanks!");
+  };
+
+  const handleFormSubmit = (event) => {
+    //TODO
+    event.preventDefault();
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+  return (
+    <div>
+      <h2>Contact Us</h2>
+      <div className="form-container">
+        <form onSubmit={handleFormSubmit}>
+          <h3>First Name</h3>
+          <label className="name">
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+              required
+              onChange={handleInputChange}
+              value={firstName}
+            />
+          </label>
+          <h3>Last Name</h3>
+          <label className="name">
+            <input
+              type="text"
+              name="lastname"
+              placeholder="Last Name"
+              required
+              onChange={handleInputChange}
+              value={lastName}
+            />
+          </label>
+          <h3>Email</h3>
+          <label className="email">
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              onChange={handleInputChange}
+              value={email}
+            />
+          </label>
+          <h3>Subject</h3>
+          <label className="subject">
+            <input
+              type="subject"
+              name="subject"
+              placeholder="Subject"
+              required
+              onChange={handleInputChange}
+              value={subject}
+            />
+          </label>
+          <h3>Message</h3>
+          <div className="message-container">
+            <textarea
+              type="text"
+              name="message"
+              placeholder="Message"
+              required
+              onChange={handleInputChange}
+              value={message}
+            />
+          </div>
+          <button type="submit" className="formbtn" onClick={handleButtonClick}>
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
